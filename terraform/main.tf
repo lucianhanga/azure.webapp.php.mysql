7ef7e2bf759002d0f5819f03684a73a5897a6260
lucian h. <lucianhanga@gmx.net>
2024-09-19 22:05:50 +0200

    add to git the moodifications to be able to create the db server. (#15)

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,11 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "4.1.0"
+      version = "~>4.1"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.0"
     }
   }
 
