97ad40635d9cc7a91e4f73e306389d16aa812006
lucian h. <lucianhanga@gmx.net>
2024-09-21 07:10:36 +0000

    commit the final changes and test the workflow

--- terraform/webapp.tf
+++ terraform/webapp.tf
@@ -1,34 +1,34 @@
 
-locals {
-    app_service_plan_name = "asp-${var.project_name}"
-    webapp_name = "webapp-${var.project_name}"    
-}
+# locals {
+#     app_service_plan_name = "asp-${var.project_name}"
+#     webapp_name = "webapp-${var.project_name}"    
+# }
 
-#
-# create the service plan for the webapp
-#
-resource "azurerm_service_plan" "webapp_serviceplan" {
-  name                = local.app_service_plan_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  os_type             = "Linux"
-  sku_name            = "F1" # Free tier
-}
+# #
+# # create the service plan for the webapp
+# #
+# resource "azurerm_service_plan" "webapp_serviceplan" {
+#   name                = local.app_service_plan_name
+#   location            = var.location
+#   resource_group_name = var.resource_group_name
+#   os_type             = "Linux"
+#   sku_name            = "F1" # Free tier
+# }
 
 
-#create the webapp using the "azurerm_linux_web_app" resource
-resource "azurerm_linux_web_app" "webapp" {
-  name                = local.webapp_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
+# #create the webapp using the "azurerm_linux_web_app" resource
+# resource "azurerm_linux_web_app" "webapp" {
+#   name                = local.webapp_name
+#   location            = var.location
+#   resource_group_name = var.resource_group_name
 
-  service_plan_id = azurerm_service_plan.webapp_serviceplan.id
-  site_config {
-    always_on = false
-    application_stack {
-        php_version = "8.1"
-    }
-  }
-}
+#   service_plan_id = azurerm_service_plan.webapp_serviceplan.id
+#   site_config {
+#     always_on = false
+#     application_stack {
+#         php_version = "8.1"
+#     }
+#   }
+# }
 
 
