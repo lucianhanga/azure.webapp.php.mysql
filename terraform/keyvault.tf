751c88a7da9d381ad3c2ccbb14e71e5bc3d8aae4
lucian h. <lucianhanga@gmx.net>
2024-09-24 21:24:11 +0000

    rename the misspelled file

--- terraform/keyvault.tf
+++ terraform/keyvault.tf
@@ -2,9 +2,6 @@ locals {
   key_vault_name = "kv-${var.project_name}" # the name of the key vault
 }
 
-# get the object id of the current user - which is the service principal
-data "azurerm_client_config" "current" {}
-
 
 # create a key vault where to store the username and password for the MySQL server
 resource "azurerm_key_vault" "key_vault" {
@@ -16,17 +13,7 @@ resource "azurerm_key_vault" "key_vault" {
   soft_delete_retention_days = 7
   purge_protection_enabled   = false
 
-  # give access to the current user service principal to the key vault
-  #   to add the username and password to the key vault for the MySQL server
-  access_policy {
-    tenant_id = var.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-    secret_permissions = ["Get", "List", "Set", "Delete", "Purge", "Recover"]
-  }
-
-  lifecycle {
-    ignore_changes = [access_policy]
-  }
+  enable_rbac_authorization = true
 
   # make sure that the webapp is created before creating the key vault 
   #   so that we can give it access to the key vault
@@ -35,28 +22,23 @@ resource "azurerm_key_vault" "key_vault" {
     ]
 }
 
+# get the object id of the current user - which is the service principal
+data "azurerm_client_config" "current" {}
 
-# give rights to the terraform service principal to read the username and password from the key vault
-resource azurerm_key_vault_access_policy "terraform" {
-  key_vault_id = azurerm_key_vault.key_vault.id
-  tenant_id = var.tenant_id
-  object_id = var.object_id
-  secret_permissions = ["Get", "List", "Set", "Delete", "Purge", "Recover"]
+resource "azurerm_role_assignment" "current" {
+  scope                = azurerm_key_vault.key_vault.id
+  role_definition_name = "Key Vault Administrator" # full access to the key vault
+  principal_id         = data.azurerm_client_config.current.object_id
 
   depends_on = [ azurerm_key_vault.key_vault ]
 }
 
-# give rights to the webapp to read the username and password from the key vault
-resource azurerm_key_vault_access_policy "webapp" {
-  key_vault_id = azurerm_key_vault.key_vault.id
-  tenant_id = var.tenant_id
-  object_id = azurerm_linux_web_app.webapp.identity[0].principal_id
-  secret_permissions = ["Get"]
+resource "azurerm_role_assignment" "webapp" {
+  scope                = azurerm_key_vault.key_vault.id
+  role_definition_name = "Key Vault Secrets User" # read access to the key vault
+  principal_id         = azurerm_linux_web_app.webapp.identity[0].principal_id
 
-  depends_on = [ 
-    azurerm_key_vault.key_vault,
-    azurerm_linux_web_app.webapp
-  ]
+  depends_on = [ azurerm_key_vault.key_vault, azurerm_linux_web_app.webapp ]
 }
 
 # add the mysql username and password to the key vault
@@ -68,6 +50,7 @@ resource "azurerm_key_vault_secret" "mysql_username" {
   # first make sure the MySQL server is created before adding the username to the key vault
   depends_on = [ 
     azurerm_key_vault.key_vault,
+    azurerm_role_assignment.current,
     local.mysql_username 
   ]
 }
@@ -80,6 +63,7 @@ resource "azurerm_key_vault_secret" "mysql_password" {
   # first make sure the MySQL server is created before adding the password to the key vault
   depends_on = [ 
     azurerm_key_vault.key_vault, 
+    azurerm_role_assignment.current,
     local.mysql_password 
   ]
 }
