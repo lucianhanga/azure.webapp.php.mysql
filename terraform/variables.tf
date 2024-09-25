2eff62b08895721de0884c81d9f28495bcf8c57a
lucian h. <lucianhanga@gmx.net>
2024-09-25 13:15:11 +0000

    use the prefix from the GitHub Environment

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -26,10 +26,16 @@ variable "project_name" {
   type        = string
 }
 
+variable "project_suffix" {
+  description = "The suffix of the project"
+  type        = string
+}
+
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
 }
+
 
 variable "object_id" {
   description = "The Object ID of the Service Principal"
